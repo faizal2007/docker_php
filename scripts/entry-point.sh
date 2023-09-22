0b409e3cbee74358a99751167b5473dfb67e636e
Mohd Faizal Mohamed Sadri <faizal@sudoers.my>
2023-09-22 20:26:58 +0800

    set dynamic document root

--- scripts/entry-point.sh
+++ scripts/entry-point.sh
@@ -5,6 +5,11 @@
 which php-fpm7.3 > /dev/null && php-fpm7.3 -D -R
 which php-fpm8.2 > /dev/null && php-fpm8.2 -D -R
 # Start Nginx
+cat <<EOF> /var/www/html/index.php
+<?php phpinfo();?>
+EOF
+
+sed -i "s|\(root\s\+\)[^;]\+;|\1${DOCUMENT_ROOT};|" /etc/nginx/sites-available/default
 nginx -c /etc/nginx/nginx.conf -g 'daemon off;'
 
 
